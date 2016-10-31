855572fb8037f4019c95be0294c571c8a3556fe9
aaronk313 <aaronykim@outlook.com>
2016-10-30 21:38:49 -0700

    Facebook API progress

---
+++ credentials.py
@@ -0,0 +1,6 @@
+passphrases = {
+    0: 'dsi dtla 2016 cohort capstone project'
+}
+
+fbtoken = 'EAACEdEose0cBAGwiCqf5BvR3uo067E0FovD8VvmaME8CFd3lggf6Vm9nRsGQTmvN0fRn3SLYFwES6gnrNgaat6bwQtDqM5ENeC9AzUJMsKSFcSCAjKwrlUZAkcP8Bo6uA4kKAh4ZCEr4QN1HnTHzEZAQIapnVXhcQ84kh4qkwZDZD'
+
