741cc62cf00295419fd417db328de1130ec5ac4b
aaronk313 <aaronykim@outlook.com>
2016-11-14 17:04:36 -0800

    monday evening commit

---
+++ credentials-Aaron’s MacBook Pro.py
@@ -0,0 +1,5 @@
+passphrases = {
+     0: 'dsi dtla 2016 cohort capstone project'
+}
+
+fbtoken = 'EAACEdEose0cBANum5sqtOISpnkea2FMUyEgN9TcP0rzl1N1buefx7qBcyRVTE61WMqnbm2twzsxTA70RNFwbcfFmNbXqnhCCoZB7lOOERC7oOrtqbhOGWuZCQ1lTCQ47ZBJ9uf8jFH4kZC7Md2dT3rlfB9RPZBTGnznYWqLIlMwZDZD'
