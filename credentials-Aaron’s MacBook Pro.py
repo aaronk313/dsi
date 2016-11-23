c91f0fc0c15e71f2f61ba169f17a229b5bf5c3a6
aaronk313 <aaronykim@outlook.com>
2016-11-23 13:44:06 -0800

    removed old files

--- credentials-Aaron’s MacBook Pro.py
+++
@@ -1,5 +0,0 @@
-passphrases = {
-     0: 'dsi dtla 2016 cohort capstone project'
-}
-
-fbtoken = 'EAACEdEose0cBANum5sqtOISpnkea2FMUyEgN9TcP0rzl1N1buefx7qBcyRVTE61WMqnbm2twzsxTA70RNFwbcfFmNbXqnhCCoZB7lOOERC7oOrtqbhOGWuZCQ1lTCQ47ZBJ9uf8jFH4kZC7Md2dT3rlfB9RPZBTGnznYWqLIlMwZDZD'
--- credentials.py
+++
@@ -1,5 +0,0 @@
-passphrases = {
-     0: 'dsi dtla 2016 cohort capstone project'
-}
-
-fbtoken = 'EAACEdEose0cBAFN0pvxm9APNvXZAP1NHmQrDIDmooIFnnuERxivy65FSsTauwz7sX1FT1KRWyR086nehAzga7TlEMaNykm4IKmqxmoZBbIqXgptV04JGC5bpgvujPhHPkFKKkbvf0ZCZBzRy2GiGO68l61gC6e23Cihf5LKfUgZDZD'
